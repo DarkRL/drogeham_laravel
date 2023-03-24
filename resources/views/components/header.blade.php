c7c50cfdce7abbe09e379ff6da9f23a0db0e6c9f
Bram <94552705+Bram-4197@users.noreply.github.com>
2023-03-24 13:55:24 +0100

    Updated Header/Navbar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,27 @@
 <div>
-    Test
+    <nav class="py-2 border-bottom bg-primary">
+        <div class="container d-flex flex-wrap text-algin align-items-center justify-content-center">
+        <img src="https://www.drogeham.com/assets/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50"/>
+        <ul class="nav mx-auto">
+
+            <li class="nav-item"><a href="#" class="nav-link link-light  active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light ">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light ">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light ">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light ">About</a></li>
+        </ul>
+        <ul class="nav">
+            <li class="nav-item"><a href="#" class="nav-link link-light ">Login</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-light ">Sign up</a></li>
+        </ul>
+        </div>
+    </nav>
+    <header class="border-bottom">
+      <section 
+        style="width: 100%;
+        height: 350px;
+        background-image: url('https://www.drogeham.com/assets/img/luchtfoto.jpg');
+        "
+      ></section>
+  </header>
 </div>
