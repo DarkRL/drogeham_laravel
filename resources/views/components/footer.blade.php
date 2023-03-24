da4c2d5610dc882728ecd7d3020b2e66147b71b2
DarkRL <sandervdg2005@gmail.com>
2023-03-24 13:28:05 +0100

    Commit de la footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,75 @@
+<footer class="bg-dark text-center text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-5">
+        <!--Grid row-->
+        <div class="row mx-5">
+            <!--Grid column-->
+            <div class="col-lg-5 col-md-12 mb-4 mb-md-0 text-white">
+                <h5>Contact</h5>
+
+                <p>
+                    Heeft u een vraag en kunt u het antwoord niet vinden op deze website? Neem dan contact met ons op.
+                </p>
+                <p>
+                    <b>Plaatselijk Belang Drogeham</b>
+                    <br>
+                    <a class="link text-white text-decoration-none" href="mailto:pbdrogeham@gmail.com">Neem contact op</a>
+                </p>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-7 col-md-12 mb-4 mb-md-0 text-white">
+                <h5>Over Drogeham</h5>
+
+                <p>
+                    Het dorp Drogeham dat lokaal ook wel 'De Ham' wordt genoemd,
+                    heeft ongeveer 1800 inwoners. Het ligt onder het Prinses Margrietkanaal
+                    in het westelijke deel van de gemeente Achtkarspelen, in het noordoosten
+                    van Friesland. Onder Drogeham vallen ook de buurtschappen: Buweklooster,
+                    Hamshorn, Hamsterpein en Westerend. Het dorp is bekend om zijn vele grote
+                    evenementen die er jaarlijks worden georganiseerd.
+                </p>
+            </div>
+            <!--Grid column-->
+        </div>
+        <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="container mb-5">
+        <div class="text-center p-3 text-white">
+            © Plaatselijk belang Drogeham | <a class="text-decoration-none text-white" href="#">Colofon & disclaimer</a> | <a class="text-decoration-none text-white" href="#">Privacy verklaring</a>
+        </div>
+        <div class="container text-center">
+            <button type="button" class="btn btn-primary mb-3" onclick="goToLink_facebook()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                </svg>
+                Facebook
+            </button>
+            <script>
+                function goToLink_facebook(){
+                    window.open('https://www.facebook.com/pbdrogeham/', '_blank');
+                }
+            </script>
+            <button type="button" class="btn btn-primary mb-3" onclick="location.href='mailto:pbdrogeham@gmail.com';">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"></path>
+                </svg>
+                E-mail
+            </button>
+            <button type="button" class="btn btn-primary mb-3" onclick="goToLink_location()">
+                <img width="16" height="16" src="https://www.drogeham.com/assets/img/place_icon.svg" alt="Plaats pictogram">
+                Locatie
+            </button>
+            <script>
+                function goToLink_location(){
+                    window.open('https://goo.gl/maps/FKkNVYGDhJY49E1j7', '_blank');
+                }
+            </script>
+        </div>
+    </div>
+    <!-- Copyright -->
+</footer>
