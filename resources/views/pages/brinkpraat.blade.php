d4d1ff74a8dc0ab45a4a8c6a6321cef05eecbf04
DarkRL <sandervdg2005@gmail.com>
2023-03-27 13:05:22 +0200

    binkpraat front-end

--- resources/views/pages/brinkpraat.blade.php
+++ resources/views/pages/brinkpraat.blade.php
@@ -15,9 +15,21 @@
     <x-header />
     <div class="container">
         <div class="row">
-            <div class="col">
-                brinkpraat pagina
+            <div class="col"></div>
+            <div class="col-10">
+                <div class="m-5">
+                    <div>
+                        <h2>Brinkpraat</h2>
+                        <p>Drogeham heeft sinds 1986 een dorpskrant met de naam Brinkpraat. Deze naam herinnert aan de vroegere brink. De inhoud bestaat uit activiteiten, geschiedenis en algemene informatie over het dorp.</p>
+                        <p>Hieronder staat een overzicht met alle edities van de Brinkpraat van 2019. Wanneer er een nieuwe editie verschenen is, zal die zo snel mogelijk hieronder gepubliceerd worden.</p>
+                        <a href="mailto:brinkpraat@chello.nl">Hier kunt u terecht met vragen en/of opmerkingen over de dorpskrant</a>
+                    </div>
+                    <div class="mt-4">
+                        <h6>FILES STAAN HIER</h6>
+                    </div>
+                </div>
             </div>
+            <div class="col"></div>
         </div>
     </div>
     <x-footer />
