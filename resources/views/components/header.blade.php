1bf30b5224fc1b5fc7e68dcc7a60370c2c6e69ac
DarkRL <sandervdg2005@gmail.com>
2023-03-27 13:24:15 +0200

    Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <nav class="py-2 bg-primary">
+    <nav class="py-2 bg-primary sticky-top">
         <div class="container d-flex flex-wrap text-algin align-items-center justify-content-center">
         <img src="https://www.drogeham.com/assets/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50"/>
         <ul class="nav mx-auto">
