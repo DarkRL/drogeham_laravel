4a8973c5c0f0bd5703ff442816460e731c4625d3
DarkRL <sandervdg2005@gmail.com>
2023-04-03 11:44:08 +0200

    Made component for all imports and added it.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="{{URL::asset("./css/app.css")}}" />
-</head>
-
+<x-imports />
 <body class="bg-dark">
     <div class="container bg-primary">
         <section class="vh-100 gradient-custom">
