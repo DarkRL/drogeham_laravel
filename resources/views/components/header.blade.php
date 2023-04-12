379617f062c083c7612104cd91dfe4c2a7932809
DarkRL <sandervdg2005@gmail.com>
2023-04-12 09:29:29 +0200

    Header and imports

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,35 +1,35 @@
-<div>
+<header>
     <nav class="py-2 bg-primary sticky-top">
         <div class="container d-flex flex-wrap text-algin align-items-center justify-content-center">
-        <img src="https://www.drogeham.com/assets/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50"/>
-        <ul class="nav mx-auto">
+            <img src="https://www.drogeham.com/assets/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50" />
+            <ul class="nav mx-auto">
 
-            <li class="nav-item"><a href="/" class="nav-link link-light " aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="/agenda" class="nav-link link-light ">Agenda</a></li>
-            <li class="nav-item"><a href="/actueel" class="nav-link link-light ">Actueel</a></li>
-            <div class="dropdown nav-link link-light">
-                <div
-                    class="dropdown-toggle"
-                    id="dropdownMenuButton"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
-                >
-                    MEYD
+                <li class="nav-item"><a href="/" class="nav-link link-light " aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="/agenda" class="nav-link link-light ">Agenda</a></li>
+                <li class="nav-item"><a href="/actueel" class="nav-link link-light ">Actueel</a></li>
+                <div class="dropdown nav-link link-light">
+                    <div class="dropdown-toggle" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+                        MEYD
+                    </div>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Hamster Hulp</a></li>
+                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Fit en Sûn</a></li>
+                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Doarpskeamer</a></li>
+                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Activiteiten 2022-2023</a></li>
+                    </ul>
                 </div>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Hamster Hulp</a></li>
-                    <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Fit en Sûn</a></li>
-                    <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Doarpskeamer</a></li>
-                    <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Activiteiten 2022-2023</a></li>
-                </ul>
-            </div>
-            <li class="nav-item"><a href="/plaatselijkbelang" class="nav-link link-light ">Plaatselijk belang</a></li>
-            <li class="nav-item"><a href="/brinkpraat" class="nav-link link-light ">Brinkpraat</a></li>
-            <li class="nav-item"><a href="/historie" class="nav-link link-light ">Historie</a></li>
-        </ul>
+                <li class="nav-item"><a href="/plaatselijkbelang" class="nav-link link-light ">Plaatselijk belang</a></li>
+                <li class="nav-item"><a href="/brinkpraat" class="nav-link link-light ">Brinkpraat</a></li>
+                <li class="nav-item"><a href="/historie" class="nav-link link-light ">Historie</a></li>
+            </ul>
         </div>
     </nav>
-    <header class="border-bottom">
-      <section id="hero-image"></section>
-  </header>
-</div>
+    <div style="align-items:center" class="d-flex parallax">
+        <div class="container h-100">
+            <div class="text-center text-white text-capitalize">
+                <h1>Plaatselijk belang drogeham</h1>
+                <button type="button" class="btn btn-primary rounded-pill">Word Lid</button>
+            </div>
+        </div>
+    </div>
+</header>
