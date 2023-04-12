19648aa0c9939780b2c061834755ca792863cb38
Bram <94552705+Bram-4197@users.noreply.github.com>
2023-04-12 10:49:38 +0200

    Changed header color

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,26 +1,25 @@
 <header>
-    <nav class="py-2 bg-primary sticky-top">
+    <nav class="py-2 bg-light sticky-top">
         <div class="container d-flex flex-wrap text-algin align-items-center justify-content-center">
-            <img src="https://www.drogeham.com/assets/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50" />
+            <img src="http://127.0.0.1:8000/img/wapen.svg" alt="Wapen Drogeham" title="Drogeham" height="50" />
             <ul class="nav mx-auto">
-
-                <li class="nav-item"><a href="/" class="nav-link link-light " aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="/agenda" class="nav-link link-light ">Agenda</a></li>
-                <li class="nav-item"><a href="/actueel" class="nav-link link-light ">Actueel</a></li>
-                <div class="dropdown nav-link link-light">
+                <li class="nav-item"><a href="/" class="nav-link link-dark " aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="/agenda" class="nav-link link-dark ">Agenda</a></li>
+                <li class="nav-item"><a href="/actueel" class="nav-link link-dark ">Actueel</a></li>
+                <div class="dropdown nav-link link-dark">
                     <div class="dropdown-toggle" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
                         MEYD
                     </div>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Hamster Hulp</a></li>
-                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Fit en Sûn</a></li>
-                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Doarpskeamer</a></li>
-                        <li><a class="dropdown-item nav-link link-light ink-hover-bg-black" href="#">Activiteiten 2022-2023</a></li>
+                        <li><a class="dropdown-item nav-link link-dark ink-hover-bg-black" href="#">Hamster Hulp</a></li>
+                        <li><a class="dropdown-item nav-link link-dark ink-hover-bg-black" href="#">Fit en Sûn</a></li>
+                        <li><a class="dropdown-item nav-link link-dark ink-hover-bg-black" href="#">Doarpskeamer</a></li>
+                        <li><a class="dropdown-item nav-link link-dark ink-hover-bg-black" href="#">Activiteiten 2022-2023</a></li>
                     </ul>
                 </div>
-                <li class="nav-item"><a href="/plaatselijkbelang" class="nav-link link-light ">Plaatselijk belang</a></li>
-                <li class="nav-item"><a href="/brinkpraat" class="nav-link link-light ">Brinkpraat</a></li>
-                <li class="nav-item"><a href="/historie" class="nav-link link-light ">Historie</a></li>
+                <li class="nav-item"><a href="/plaatselijkbelang" class="nav-link link-dark ">Plaatselijk belang</a></li>
+                <li class="nav-item"><a href="/brinkpraat" class="nav-link link-dark ">Brinkpraat</a></li>
+                <li class="nav-item"><a href="/historie" class="nav-link link-dark ">Historie</a></li>
             </ul>
         </div>
     </nav>
