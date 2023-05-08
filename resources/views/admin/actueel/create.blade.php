622e09cf7083b1a1cd7e3c9800d1c6232b322095
quickrick2 <rick.kampstra2002@gmail.com>
2023-05-08 12:03:01 +0200

    Editing now works (I think)

--- resources/views/admin/actueel/create.blade.php
+++ resources/views/admin/actueel/create.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 
-<form action="" method="post" class="form-group">
-    <h6>Nieuw Artikel</h6>
-    @csrf
-    <input name="headline" class="form-control" placeholder="Titel"></input>
-    <textarea name="fulltext" class="form-control" id="file-picker"></textarea>
-    <button type="submit" value="Save" class="btn btn-primary">Toevoegen</button>
-</form>
+<div class="row justify-content-center mt-5">
+    <div class="col-md-8">
+        <form action="" method="post" class="form-group">
+            <h6>Nieuw Artikel</h6>
+            @csrf
+            <input name="headline" class="form-control" placeholder="Titel"></input>
+            <textarea name="fulltext" class="form-control" id="file-picker"></textarea>
+            <button type="submit" value="Save" class="btn btn-primary">Toevoegen</button>
+        </form>
+    </div>
+</div>
 
 @endsection
--- resources/views/admin/actueel/edit.blade.php
+++ resources/views/admin/actueel/edit.blade.php
@@ -1,6 +1,18 @@
-<form action="" method="post" class="form-group">
-    @csrf
-    @method('PATCH')
-    <textarea name="fulltext" class="form-control" id="file-picker">{{ $post->fulltext }}</textarea>
-    <button type="submit" value="Update" class="btn btn-primary">Submit</button>
-</form>
+@extends('admin.layouts')
+
+@section('content')
+
+<div class="row justify-content-center mt-5">
+    <div class="col-md-8">
+        <form action="" method="post" class="form-group">
+            <h6>Artikel Aanpassen</h6>
+            @csrf
+            @method('PATCH')
+            <input name="headline" class="form-control" placeholder="Titel" value="{{ $post->headline }}"></input>
+            <textarea name="fulltext" class="form-control" id="file-picker">{{ $post->fulltext }}</textarea>
+            <button type="submit" value="Update" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+
+@endsection
