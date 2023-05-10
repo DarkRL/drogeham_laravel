0c6bf1dcde6ab4185f737dd5aec434480437c2ae
DarkRL <sandervdg2005@gmail.com>
2023-05-10 15:22:50 +0200

    Added whole cms for plaatselijkbelang

--- app/Http/Controllers/Admin/PlaatselijkBelangPostController.php
+++ app/Http/Controllers/Admin/PlaatselijkBelangPostController.php
@@ -3,50 +3,49 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\admin\HomePost;
+use App\Models\admin\PlaatselijkbelangPosts;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class PlaatselijkBelangController extends Controller
+class PlaatselijkBelangPostController extends Controller
 {
     public function index()
     {
-        // $posts = HomePost::all();
         $posts = DB::select('SELECT * FROM plaatselijkbelang_posts WHERE id = ?' , ['1']);
         return view('admin.plaatselijkbelang.index', ['posts' => $posts]);
     }
 
     public function store(Request $request)
     {
-        $newPost = HomePost::create([
+        $newPost = PlaatselijkbelangPosts::create([
             'fulltext' => $request->fulltext,
             'datetime' => date("Y-m-d H:m:s")
         ]);
-        return redirect()->route('admin.home.index')
+        return redirect()->route('admin.plaatselijkbelang.index')
             ->withSuccess('Nieuwe inhoud is succesvol aangemaakt!');
     }
 
     public function create()
     {
         $id = 1;
-        if (HomePost::where('id', $id)->exists()) {
-            return redirect()->route('admin.home.index');
+        if (PlaatselijkbelangPosts::where('id', $id)->exists()) {
+            return redirect()->route('admin.plaatselijkbelang.index');
         }
-        return view('admin.home.create');
+        return view('admin.plaatselijkbelang.create');
     }
 
-    public function edit(HomePost $id)
+    public function edit(PlaatselijkbelangPosts $id)
     {
-        return view('admin.home.edit', ['post' => $id]);
+        return view('admin.plaatselijkbelang.edit', ['post' => $id]);
     }
 
     public function update(Request $request, $id)
     {
-        HomePost::find($id)->update([
+        PlaatselijkbelangPosts::find($id)->update([
             'fulltext' => $request->fulltext
         ]);
-        return redirect()->route('admin.home.index')
+        return redirect()->route('admin.plaatselijkbelang.index')
             ->withSuccess('De inhoud is succesvol aangepast!');
     }
 }
