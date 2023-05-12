9fda2b174d7bbb52e465e944717b59f3f0485e3d
DarkRL <sandervdg2005@gmail.com>
2023-05-12 13:54:36 +0200

    Added tinymce locally instead of api.

--- resources/views/components/import/tinymce-config.blade.php
+++ resources/views/components/import/tinymce-config.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdn.tiny.cloud/1/cvn8qonvus0vl48pcypw8d8wfphd1w3kqcu1p87koja5uwb9/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="{!! URL::asset('assets/tinymce/js/tinymce/tinymce.min.js') !!}"></script>
 <script>
     tinymce.init({
         selector: 'textarea#file-picker',
@@ -6,6 +6,7 @@
         menubar: 'edit insert view format table tools',
         toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | image link',
         autoresize_bottom_margin: 50,
+        promotion: false,
         branding: false,
         language: 'nl',
         image_title: true,
