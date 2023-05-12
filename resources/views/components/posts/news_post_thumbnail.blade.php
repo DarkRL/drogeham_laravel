afeb9fb6c1f3147af0292a1bff89880383c8dc4c
quickrick2 <rick.kampstra2002@gmail.com>
2023-05-12 11:38:39 +0200

    Update news_post_thumbnail.blade.php

--- resources/views/components/posts/news_post_thumbnail.blade.php
+++ resources/views/components/posts/news_post_thumbnail.blade.php
@@ -1,10 +1,10 @@
 <section class="col-4">
     <div class="card mb-4">
         <div class="card-body">
-            <a href="{{ URL('/templates/'. $postid .'/newspost') }}">
+            <a href="{{ URL('/templates/'. $postid .'/newspost') }}" class="" style="color: black; text-decoration: none">
                 <img src="{{URL::asset("./img/luchtfoto.jpg")}}" class="img-thumbnail" alt="thumbnail">
                 <h4>{!! html_entity_decode($headline) !!}</h4>
-                <h6>{!! html_entity_decode($datetime) !!}</h6>
+                <p>{!! html_entity_decode($datetime) !!}</p>
             </a>
         </div>
     </div>
