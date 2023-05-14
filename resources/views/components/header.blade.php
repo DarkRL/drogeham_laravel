254c9331c5133c53727dabbed0a035eb01417a0f
DarkRL <sandervdg2005@gmail.com>
2023-05-14 17:14:41 +0200

    Added underline animation

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,10 +3,10 @@
         <div class="container d-flex flex-wrap text-algin align-items-center justify-content-center">
             <a href={{route('page', ['page' => 'home'])}}><img src="{{URL::asset("./img/wapen.svg")}}" alt="Wapen Drogeham" title="Drogeham" height="50" /></a>
             <ul class="nav mx-auto">
-                <li class="nav-item"><a href={{route('page', ['page' => 'home'])}} class="nav-link link-dark " aria-current="page">Home</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'agenda'])}} class="nav-link link-dark ">Agenda</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'actueel'])}} class="nav-link link-dark ">Actueel</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'projecten'])}} class="nav-link link-dark ">Projecten</a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'home'])}}" class="nav-link link-dark " aria-current="page"><span class="hover-underline-animation hover-underline-grey">Home</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'agenda'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Agenda</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'actueel'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Actueel</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'projecten'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Projecten</span></a></li>
                 <div class="dropdown nav-link link-dark">
                     <a class="text-decoration-none text-reset" href="{{route('page', ['page' => 'meyd'])}}">
                         <div class="dropdown-toggle" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
@@ -20,10 +20,10 @@
                         <li><a class="dropdown-item nav-link link-dark ink-hover-bg-black" href="#">Activiteiten 2022-2023</a></li>
                     </ul>
                 </div>
-                <li class="nav-item"><a href={{route('page', ['page' => 'plaatselijkbelang'])}} class="nav-link link-dark ">Plaatselijk belang</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'brinkpraat'])}} class="nav-link link-dark ">Brinkpraat</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'historie'])}} class="nav-link link-dark ">Historie</a></li>
-                <li class="nav-item"><a href={{route('page', ['page' => 'contact'])}} class="nav-link link-dark ">Contact</a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'plaatselijkbelang'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Plaatselijk belang</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'brinkpraat'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Brinkpraat</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'historie'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Historie</span></a></li>
+                <li class="nav-item"><a href="{{route('page', ['page' => 'contact'])}}" class="nav-link link-dark "><span class="hover-underline-animation hover-underline-grey">Contact</span></a></li>
             </ul>
             <a href={{route('page', ['page' => 'meyd'])}}><img src="{{URL::asset("./img/MEYD_drogeham.png")}}" alt="Logo MEYD" title="MEYD" height="60" /></a>
         </div>
