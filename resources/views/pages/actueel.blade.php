d29270bcdaa3513b3c26ee9c7069168e65ed3640
DarkRL <sandervdg2005@gmail.com>
2023-06-17 16:15:04 +0200

    Actueel page styling finished

--- resources/views/pages/actueel.blade.php
+++ resources/views/pages/actueel.blade.php
@@ -20,19 +20,18 @@
 
         $chunkSize = ceil(count($startingArray) / 3);
 
-        for ($i = 0; $i < count($startingArray); $i++) { $sortedArrays[$i % 3][]=$startingArray[$i]; } 
-        @endphp 
-        @foreach ($sortedArrays as $chunks_arrays) 
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        for ($i = 0; $i < count($startingArray); $i++) { $sortedArrays[$i % 3][]=$startingArray[$i]; } @endphp @foreach ($sortedArrays as $chunks_arrays) <div class="col-lg-4 mb-4 mb-lg-0">
             @foreach ($chunks_arrays as $post)
 
             <div class="position-relative">
-                <div class="d-flex align-items-end">
+                <div class="d-flex align-items-end custom-badge">
                     <a class="mt-5 mb-3" href="{{ route('templates.newspost', ['id' => $post->id]) }}">
                         <img src="{{ $post->photo }}" class="img-fluid rounded" alt="thumbnail">
 
-                        <div class="position-absolute bottom-0 start-50 translate-middle-x bg-light badge w-75 shadow-lg p-3">
-                            <div class="text-dark h6 text-top-left hideOverflow">{!! html_entity_decode($post->headline) !!}</div>
+                        <div class="custom-text">
+                            <div class="position-absolute bottom-0 start-50 translate-middle-x bg-light badge w-75 shadow-lg p-3" style="transform: translateZ(0);">
+                                <div class="text-dark h6 text-top-left hideOverflow">{!! html_entity_decode($post->headline) !!}</div>
+                            </div>
                         </div>
                     </a>
                 </div>
@@ -40,9 +39,9 @@
 
 
             @endforeach
-        </div>
-        @endforeach
     </div>
+    @endforeach
+</div>
 </div>
 
 <div class="d-flex justify-content-between align-items-center">
