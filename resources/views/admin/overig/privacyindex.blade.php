965931fc9de4f2876d686b231cabcabe391db0f8
DarkRL <sandervdg2005@gmail.com>
2023-06-26 11:42:40 +0200

    removed thing

--- resources/views/admin/overig/privacyindex.blade.php
+++ resources/views/admin/overig/privacyindex.blade.php
@@ -18,24 +18,18 @@
             </a>
             @endif
         </h5>
-
-        <div class="card mt-3">
-            <div class="card-header">Feedback</div>
-            <div class="card-body">
-                @if ($message = Session::get('success'))
-                <div class="alert alert-success">
-                    {{ $message }}
-                </div>
-                @elseif ($message = Session::get('fail'))
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-                @endif
-            </div>
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            {{ $message }}
         </div>
+        @elseif ($message = Session::get('fail'))
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @endif
     </div>
 </div>
-<div class="row justify-content-center mt-5">
+<div class="row justify-content-center mt-2">
     <div class="col-md-10">
         <div class="card">
             <div class="card-header">
