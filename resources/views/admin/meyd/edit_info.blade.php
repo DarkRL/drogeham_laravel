dc0afdca84a0730cab44898e3ba6fdc2d9a63095
DarkRL <sandervdg2005@gmail.com>
2023-07-18 14:35:01 +0200

    commit de la vakantie

--- resources/views/admin/meyd/edit_info.blade.php
+++ resources/views/admin/meyd/edit_info.blade.php
@@ -8,7 +8,7 @@
             <h6>Meyd informatiepagina Aanpassen</h6>
             @csrf
             @method('PATCH')
-            <textarea name="fulltext" class="form-control" id="file-picker" required>{{ $post->fulltext }}</textarea>
+            <textarea name="fulltext" class="form-control" id="file-picker">{{ $post->fulltext }}</textarea>
             <a href="{{ url()->previous() }}"><button type="button" class="btn btn-secondary">Annuleren</button></a>
             <button type="submit" value="Update" class="btn btn-primary">Submit</button>
         </form>
